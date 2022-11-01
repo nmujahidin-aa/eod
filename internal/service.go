5fa8963d844cb524f0b93ce8a9fb0b8a82c025b5
Galih Anggi Satriawan <satriawan.galih99@gmail.com>
2022-11-01 22:45:52 +0700

    revised sequence

--- internal/service.go
+++ internal/service.go
@@ -27,6 +27,7 @@ func (s *service) EndOfDay(ctx context.Context) error {
 		return err
 	}
 	var afterEodList AfterEodList
+	// copy data to after eod
 	for _, beforeEod := range beforeEodList {
 		afterEod := NewAfterEod(*beforeEod)
 		afterEodList = append(afterEodList, afterEod)
@@ -34,59 +35,6 @@ func (s *service) EndOfDay(ctx context.Context) error {
 
 	var wg sync.WaitGroup
 	mutex := sync.Mutex{}
-	workerPoolProcessOne := utils.NewWorkerPool(ctx, 10)
-	workerPoolProcessOne.Run()
-	for index, beforeEod := range beforeEodList {
-		wg.Add(1)
-		tempIndex := index
-		go workerPoolProcessOne.AddTask(func(workerId int32) {
-			defer wg.Done()
-			mutex.Lock()
-			afterEodList[tempIndex].AverageBalanced = (beforeEod.Balanced + beforeEod.PreviousBalanced) / 2
-			afterEodList[tempIndex].ProcessOne = int64(workerId)
-			mutex.Unlock()
-		})
-	}
-	wg.Wait()
-	workerPoolProcessOne.Stop()
-
-	workerPoolProcessTwoA := utils.NewWorkerPool(ctx, 10)
-	workerPoolProcessTwoA.Run()
-
-	workerPoolProcessTwoB := utils.NewWorkerPool(ctx, 10)
-	workerPoolProcessTwoB.Run()
-
-	for index, beforeEod := range beforeEodList {
-		wg.Add(1)
-		tempIndex := index
-		if beforeEod.Balanced <= 150 {
-			go workerPoolProcessTwoA.AddTask(func(workerId int32) {
-				defer wg.Done()
-				mutex.Lock()
-				afterEodList[tempIndex].FreeTransfer = 5
-				afterEodList[tempIndex].ProcessTwoA = func() *int64 {
-					workerId64 := int64(workerId)
-					return &workerId64
-				}()
-				mutex.Unlock()
-			})
-			continue
-		}
-		go workerPoolProcessTwoB.AddTask(func(workerId int32) {
-			defer wg.Done()
-			mutex.Lock()
-			afterEodList[tempIndex].Balanced += 25
-			afterEodList[tempIndex].ProcessTwoB = func() *int64 {
-				workerId64 := int64(workerId)
-				return &workerId64
-			}()
-			mutex.Unlock()
-		})
-	}
-	wg.Wait()
-	workerPoolProcessTwoA.Stop()
-	workerPoolProcessTwoB.Stop()
-
 	workerPoolProcessThree := utils.NewWorkerPool(ctx, 8)
 	workerPoolProcessThree.Run()
 	for index := 0; index < 100; index++ {
@@ -105,6 +53,62 @@ func (s *service) EndOfDay(ctx context.Context) error {
 	}
 	wg.Wait()
 	workerPoolProcessThree.Stop()
+
+	workerPoolProcessTwoA := utils.NewWorkerPool(ctx, 10)
+	workerPoolProcessTwoA.Run()
+
+	workerPoolProcessTwoB := utils.NewWorkerPool(ctx, 10)
+	workerPoolProcessTwoB.Run()
+
+	for _, afterEod := range afterEodList {
+		wg.Add(1)
+		tempAfterEod := afterEod
+		if afterEod.Balanced > 150 {
+			go workerPoolProcessTwoB.AddTask(func(workerId int32) {
+				defer wg.Done()
+				mutex.Lock()
+				tempAfterEod.Balanced += 25
+				tempAfterEod.ProcessTwoB = func() *int64 {
+					workerId64 := int64(workerId)
+					return &workerId64
+				}()
+				mutex.Unlock()
+			})
+		}
+
+		if afterEod.Balanced <= 150 {
+			go workerPoolProcessTwoA.AddTask(func(workerId int32) {
+				defer wg.Done()
+				mutex.Lock()
+				tempAfterEod.FreeTransfer = 5
+				tempAfterEod.ProcessTwoA = func() *int64 {
+					workerId64 := int64(workerId)
+					return &workerId64
+				}()
+				mutex.Unlock()
+			})
+		}
+
+	}
+	wg.Wait()
+	workerPoolProcessTwoA.Stop()
+	workerPoolProcessTwoB.Stop()
+
+	workerPoolProcessOne := utils.NewWorkerPool(ctx, 10)
+	workerPoolProcessOne.Run()
+	for _, afterEod := range afterEodList {
+		wg.Add(1)
+		tempAfterEod := afterEod
+		go workerPoolProcessOne.AddTask(func(workerId int32) {
+			defer wg.Done()
+			mutex.Lock()
+			tempAfterEod.AverageBalanced = (tempAfterEod.Balanced + tempAfterEod.PreviousBalanced) / 2
+			tempAfterEod.ProcessOne = int64(workerId)
+			mutex.Unlock()
+		})
+	}
+	wg.Wait()
+	workerPoolProcessOne.Stop()
 	err = s.repo.UpdateAfterEod(ctx, afterEodList)
 	if err != nil {
 		log.Println("[Service][EndOfDay] update after eod failed", err)
